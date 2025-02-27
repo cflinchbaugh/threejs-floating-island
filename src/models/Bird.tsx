5a6b6d2215099b494744a550c6aaa6c5c8e2933c
Chris Flinchbaugh <cflinchbaugh@Chriss-MacBook-Pro.local>
2025-02-26 20:16:12 -0500

    Remove unused var

--- src/models/Bird.tsx
+++ src/models/Bird.tsx
@@ -2,7 +2,7 @@ import { useGLTF } from "@react-three/drei";
 
 import birdScene from "../assets/3d/bird.glb";
 const Bird = () => {
-  const { scene, animations } = useGLTF(birdScene);
+  const { scene } = useGLTF(birdScene);
   return (
     <mesh position={[0, 2, 1]} scale={[0.003, 0.003, 0.003]}>
       <primitive object={scene} />
